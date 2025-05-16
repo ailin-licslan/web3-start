d0a6b6b7edae79fc17000f3073a11083ec05d01b
licslan <licslan@sina.com>
2025-05-16 12:14:48 +0800

    testing

--- bank-contract/ignition/modules/deploy.js
+++ bank-contract/ignition/modules/deploy.js
@@ -5,15 +5,15 @@ async function main() {
     console.log("Deploying contracts with the account:", deployer.address);
 
     // 部署 Z0Token
-    const Z0Token = await ethers.getContractFactory("Z0Token");
-    const z0Token = await Z0Token.deploy();
-    await z0Token.waitForDeployment();
+    const Z0Token = await ethers.getContractFactory("Zero");
+    const z0Token = await Z0Token.deploy("ZERO","Z0")
+    await z0Token.waitForDeployment;
     console.log("Z0Token deployed to:", z0Token.address);
 
     // 部署 Bank
     const Bank = await ethers.getContractFactory("Bank");
     const bank = await Bank.deploy(z0Token.address);
-    await bank.waitForDeployment();
+    await bank.waitForDeployment;
     console.log("Bank deployed to:", bank.address);
 }
 
@@ -22,7 +22,7 @@ main().catch((error) => {
     process.exitCode = 1;
 });
 
-//npx hardhat node
+//npx hardhat node  TODO not work yet
 //npx hardhat ignition deploy ./ignition/modules/DeployZero.js --network localhost
 //npx hardhat ignition deploy ./ignition/modules/DeployBank.js --network localhost
-//npx hardhat run scripts/deploy.js --network localhost
+//npx hardhat run ./ignition/modules/deploy.js --network localhost
