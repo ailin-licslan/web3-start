78679fd8b95f733ea562ee6f3c65deb954165adc
licslan <licslan@sina.com>
2025-05-17 13:19:03 +0800

    demo

--- bank-client/src/main.tsx
+++ bank-client/src/main.tsx
@@ -2,7 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './App.css'
 /*<script src="https://cdn.tailwindcss.com"></script>*/
-import App from './AppStarting.tsx'
+
+
+// 下面如果演示功能请切换到 AppStarting.tsx
+import App from './AppStarting2.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
